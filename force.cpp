75b3eae458a1756e42045308b65f9fd57d5de95d
Johannes <maas@dmitry.amrl.uakron.edu>
2014-06-21 10:44:39 -0400

    Added vdW force calculation and neighbor list

--- force.cpp
+++ force.cpp
@@ -7,10 +7,11 @@
 
 #include "force.h"
 
-//define function protoypes
-//note: for force calculations, calculate forces between x,y,z coordinates seperately
-realtype calcRegularSpring(realtype[],realtype,realtype);
-realtype calcTorsionalSpring(realtype,realtype[]);
+void generate_nhbd( N_Vector& u,
+                    std::vector<int>& nhbd,
+                    std::vector<int>& nhbd_partner,
+                    realtype r,
+                    int numPoints);
 
 extern "C" int force(realtype t, N_Vector u, N_Vector udot, void *user_data) 
 {
@@ -23,7 +24,7 @@ extern "C" int force(realtype t, N_Vector u, N_Vector udot, void *user_data)
 
 
 	//NOTE: assigment of spring points should be in struct; this is a temporary assignment for testing purposes
-	/*int espringlist[] = { 0,1,0,1,2,0,2,3,0,3,4,
+	int espringlist[] = { 0,1,0,1,2,0,2,3,0,3,4,
 			       	       0,4,5,0,5,0,0,0,6,1,1,6,1,2,6,1,3,6,1,4,6,1,5,
 				       6,1,5,10,0,10,9,0,8,7,0,7,4,0,5,11,1,10,11,1,
 				       9,11,1,8,11,1,7,11,1,4,11,1};
@@ -31,7 +32,7 @@ extern "C" int force(realtype t, N_Vector u, N_Vector udot, void *user_data)
 	int tspringlist[] = { 1,6,0,5,1,0,1,6,2,1,1,2,6,3,1,2,3,6,4,
 						1,3,4,6,5,0,4,5,6,0,1,6,4,5,11,0,4,11,
 						5,10,1,5,11,10,9,1,10,11,9,8,1,9,11,
-						8,7,1,8,11,7,4,1 };*/
+						8,7,1,8,11,7,4,1 };
 	 
 	
 	//INITIALIZE UDOT
@@ -51,8 +52,6 @@ extern "C" int force(realtype t, N_Vector u, N_Vector udot, void *user_data)
 			int y2index = x2index+numPoints;
 			int z1index = x1index+2*(numPoints);
 			int z2index = x2index+2*(numPoints);
-
-			std::cout << x1index << " " << y1index;
 
 			realtype x1 = NV_Ith_S(u,x1index);
 			realtype x2 = NV_Ith_S(u,x2index);
@@ -84,10 +83,10 @@ extern "C" int force(realtype t, N_Vector u, N_Vector udot, void *user_data)
 	{
 		if (i % test.forceIndexT == 4) {
 			//get x,y,z coords of points i, j, k, m
-			int xiindex = espringlist[i-4];
-			int xjindex = espringlist[i-3];
-			int xkindex = espringlist[i-2];
-			int xmindex = espringlist[i-1];
+			int xiindex = tspringlist[i-4];
+			int xjindex = tspringlist[i-3];
+			int xkindex = tspringlist[i-2];
+			int xmindex = tspringlist[i-1];
 
 			int yiindex = xiindex+numPoints;
 			int yjindex = xjindex+numPoints;
@@ -99,44 +98,103 @@ extern "C" int force(realtype t, N_Vector u, N_Vector udot, void *user_data)
 			int zkindex = xkindex+numPoints*2;
 			int zmindex = xmindex+numPoints*2;
 
-			realtype x1 = NV_Ith_S(u,x1index);
-			realtype x2 = NV_Ith_S(u,x2index);
+			realtype xi = NV_Ith_S(u,xiindex);
+			realtype xj = NV_Ith_S(u,xjindex);
+			realtype xk = NV_Ith_S(u,xkindex);
+			realtype xm = NV_Ith_S(u,xmindex);
 
-			realtype y1 = NV_Ith_S(u,y1index);
-			realtype y2 = NV_Ith_S(u,y2index);
+			realtype yi = NV_Ith_S(u,yiindex);
+			realtype yj = NV_Ith_S(u,yjindex);
+			realtype yk = NV_Ith_S(u,ykindex);
+			realtype ym = NV_Ith_S(u,ymindex);
 
-			realtype z1 = NV_Ith_S(u,z1index);
-			realtype z2 = NV_Ith_S(u,z2index);
+			realtype zi = NV_Ith_S(u,ziindex);
+			realtype zj = NV_Ith_S(u,zjindex);
+			realtype zk = NV_Ith_S(u,zkindex);
+			realtype zm = NV_Ith_S(u,zmindex);
+
+			realtype normgrad = (xi + xj - 2*xh)*(yi*yi+zi*zj)
+		+ (xi - xj)*((yj*yh-yi*yj)+(zj*zh-zi*zh))
+		+ (x1 - x3)*(y2*y2 + z2*z2)
+		+ (x1 - x2)*(y3*y3 + z3*z3);
 		}
 	}*/
 
-	//to be done: van der Waals
+	//van der Waals
+	//generate neighbor list every 10th step
+	realtype radius = test.r;
+	int curtime;
+	curtime = static_cast<int> (t);
+	if (curtime % 10 == 0){
+		generate_nhbd(u, test.nhbd, test.nhbd_partner, radius, numPoints);
+	}
 
+	//compute bending
+	int isize = test.nhbd.size();
+	realtype eps = test.epsilon;
+	realtype del = test.delta;
+
+	for (int i = 0; i < isize; ++i) {
+		int x1index = test.nhbd.at(i);
+		int x2index = test.nhbd_partner.at(i);
+		int y1index = x1index+numPoints;
+		int y2index = x2index+numPoints;
+		int z1index = x1index+2*(numPoints);
+		int z2index = x2index+2*(numPoints);
+
+		realtype x = NV_Ith_S(u,x1index) - NV_Ith_S(u,x2index);
+		realtype y = NV_Ith_S(u,y1index) - NV_Ith_S(u,y2index);
+		realtype z = NV_Ith_S(u,z1index) - NV_Ith_S(u,z2index);
+
+		realtype LJdist = sqrt( x*x + y*y + z*z );
+		realtype LJconst = RCONST(-12)*eps/del;
+		realtype LJprime = pow(del/LJdist, 13) - pow(del/LJdist, 7);
+		realtype LJx = LJconst*LJprime*x/LJdist;
+		realtype LJy = LJconst*LJprime*y/LJdist;
+		realtype LJz = LJconst*LJprime*z/LJdist;
+
+		NV_Ith_S(udot, x1index) += LJx;
+		NV_Ith_S(udot, x2index) -= LJx;
+		NV_Ith_S(udot, y1index) += LJy;
+		NV_Ith_S(udot, y2index) -= LJy;
+		NV_Ith_S(udot, z1index) += LJz;
+		NV_Ith_S(udot, z2index) -= LJz;
+
+	}
 
 }
 
-
-//returns gradient for x coordinate; to get y or z gradient, just input in different order
-realtype calcTorsionalSpring(realtype kb, realtype pointslist[])
+void generate_nhbd( N_Vector& u,
+                    std::vector<int>& nhbd,
+                    std::vector<int>& nhbd_partner,
+                    realtype r,
+                    int numPoints)
 {
-	realtype x1 = pointslist[0];
-	realtype x2 = pointslist[1];
-	realtype x3 = pointslist[2];
-	realtype x4 = pointslist[3];
-	realtype y1 = pointslist[4];
-	realtype y2 = pointslist[5];
-	realtype y3 = pointslist[6];
-	realtype y4 = pointslist[7];
-	realtype z1 = pointslist[8];
-	realtype z2 = pointslist[9];
-	realtype z3 = pointslist[10];
-	realtype z4 = pointslist[11];
-	realtype normgrad = (x3 + x2 - 2*x4)*(y3*y2+z3*z2)
-		+ (x3 - x2)*((y1*y2-y3*y2)+(z1*z2-z3*z1))
-		+ (x1 - x3)*(y2*y2 + z2*z2)
-		+ (x1 - x2)*(y3*y3 + z3*z3);
-	realtype gradient = -kb * normgrad;
+	nhbd.clear();
+	nhbd_partner.clear();
 
-	return gradient;
+	realtype r2 = r*r;
+
+	for (int i = 0; i < numPoints - 1; ++i)
+	{
+		realtype x1 = NV_Ith_S(u, i);
+		realtype y1 = NV_Ith_S(u, i+numPoints);
+		realtype z1 = NV_Ith_S(u, i+2*numPoints);
+
+
+		for (int j = i+1; j < numPoints; ++j)
+		{
+			realtype x2 = NV_Ith_S(u, j);
+			realtype y2 = NV_Ith_S(u, j+numPoints);
+			realtype z2 = NV_Ith_S(u, j+2*numPoints);
+
+			//calculate distance between points
+			realtype dist2 = (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2);
+			if (dist2 <= r2) {
+				nhbd.push_back(i);
+				nhbd_partner.push_back(j);
+			}
+		}
+	}
+
 }
-
--- force.h
+++ force.h
@@ -12,6 +12,7 @@
 
 
 extern "C" int force(realtype t, N_Vector u, N_Vector udot, void *user_data);
+
 //Structure for configuration of hexagons. 
 struct hexconfig
 {
@@ -21,20 +22,33 @@ struct hexconfig
 	int forceIndexE, forceIndexT;
 	std::vector<int> espringlist;
 	int * tspringlist;
-	int espringsize, tspringsize;
+	int espringsize, tspringsize; 
 
-	/*std::vector<int> nhbd_hex;
-	std::vector<int> nhbd_partner;*/
+	//radius for generating neighbor list
+	realtype r;
 
+	//Lennard-Jones constants
+	realtype delta, epsilon;
 
-	hexconfig(N_Vector& init) : espringlist({ 0,1,0,1,2,0,2,3,0,3,4,
-			       	       0,4,5,0,5,0,0,0,6,1,1,6,1,2,6,1,3,6,1,4,6,1,5,
+	int numPoints;
+
+	std::vector<int> nhbd;
+	std::vector<int> nhbd_partner;
+
+	hexconfig(N_Vector& init) 
+	:espringlist({ 0,1,0,1,2,0,2,3,0,3,4,
+			       0,4,5,0,5,0,0,0,6,1,1,6,1,2,6,1,3,6,1,4,6,1,5,
 				       6,1,5,10,0,10,9,0,8,7,0,7,4,0,5,11,1,10,11,1,
 				       9,11,1,8,11,1,7,11,1,4,11,1})
 	 {
+	 	r = 0.1;
+	 	numPoints = 12;
 		kb = 0;
 		ks = 100;
 		l = 0.5;
+		delta = 0.2;
+		epsilon = 6;
+
 		forceIndexE = 3;
 		forceIndexT = 5;
 		//regular springs: list two points and bond type
